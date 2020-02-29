7f786067ee3541f2adfe62c2c02d90a33dc6c27f
Maksym Rohachov <romax982@gmail.com>
2020-02-29 23:35:56 +0200

    Initial commit of the whole project

---
+++ hotel-reservation-website/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace hotel_reservation_website.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
